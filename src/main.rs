59f916c44cf1fcf0e7d6f6343c2b83de7c8cb548
Eamon <eamon.r.burns@gmail.com>
2023-11-24 14:07:46 -0800

    - Updated `eframe`, improved UI - Updated `eframe` from 0.14.0 to 0.24.0 Added calculation output, and groups around the `History` and `Assignments` sections Groups fill full available height, width Now handles and displays errors

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,31 @@ use std::collections::HashMap;
 
 use eframe::{
     run_native,
-    epi::{App, Frame},
-    egui::{CentralPanel, CtxRef},
-    NativeOptions
+    App,
+    Frame,
+    NativeOptions,
+    egui::{
+        CentralPanel,
+        Context,
+        Key,
+        Layout,
+        Align,
+        Direction
+    },
 };
 
-use m_calc::{calc::Token, calculate, tokenize, display_expr};
+use m_calc::{
+    calculate,
+    tokenize,
+    display_expr,
+    Token,
+    CalcErr,
+};
 
 struct Calculator {
     context: HashMap<String, Vec<Token>>,
     input_text: String,
+    output_text: String,
     history: Vec<Vec<Token>>,
 }
 impl Calculator {
@@ -19,48 +34,79 @@ impl Calculator {
         Calculator {
             context: HashMap::new(),
             input_text: String::new(),
+            output_text: String::new(),
             history: vec![vec![]],
         }
     }
 }
 impl App for Calculator {
-    fn update(&mut self, ctx: &CtxRef, frame: &mut Frame<'_>) {
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.text_edit_singleline(&mut self.input_text);
-                if ui.button("This is a button").clicked() {
+            ui.horizontal(|ui| { // TODO: Make contents horizontally justified
+                let input = ui.text_edit_singleline(&mut self.input_text);
+                let button = ui.button("Calculate");
+                if button.clicked() || ui.input(|i| i.key_pressed(Key::Enter)) {
+                    // Check if the button was clicked _or_ the `Enter` was pressed
                     println!("Input text: `{}`", self.input_text);
-                    let tokens = tokenize((self.input_text.clone() + " ").as_str()).unwrap();
 
-                    self.history.push(tokens.clone());
+                    match tokenize((self.input_text.clone()).as_str()) {
+                        Ok(tokens) => {
+                            self.history.push(tokens.clone());
 
-                    let res = calculate(&tokens, &mut self.context).unwrap();
-                    println!("Tokens: `{:?}` (`{}`)", tokens, display_expr(&tokens));
-                    println!("Result: `{}` (`{}`)", res, display_expr(&vec![res.clone()]));
+                            match calculate(&tokens, &mut self.context) {
+                                Ok(res) => {
+                                    self.output_text = res.display(); // Output result
+                                },
+                                Err(CalcErr(err_str)) => self.output_text = err_str, // Output error
+                            }
+                        },
+                        Err(CalcErr(err_str)) => {
+                            self.output_text = err_str;
+                        },
+                    }
+
+                    // println!("Tokens: `{:?}` (`{}`)", tokens, display_expr(&tokens));
+                    // println!("Result: `{}` (`{}`)", res, display_expr(&vec![res.clone()]));
+                    input.request_focus(); // Re-focus after (`Enter` un-focuses)
                 }
+                ui.set_width(ui.available_width());
             });
 
+            ui.add_space(5.0);
+            ui.group(|ui| { // Output
+                ui.label(&self.output_text);
+                ui.set_width(ui.available_width());
+            });
+
+            // TODO: Make columns scrollable
+            ui.add_space(5.0);
             ui.columns(2, |cols| {
-                cols[0].heading("History");
-                for expr in self.history.iter().rev() {
-                    cols[0].monospace(display_expr(expr));
-                }
-                cols[1].heading("Assignments");
-                for entry in &self.context {
-                    cols[1].monospace(format!("{} = {}", entry.0, display_expr(entry.1)));
-                }
+                cols[0].group(|ui| {
+                    ui.heading("History");
+                    for expr in self.history.iter().rev() {
+                        ui.monospace(display_expr(expr));
+                    }
+                    ui.set_height(ui.available_height());
+                });
+                cols[0].set_min_height(cols[0].available_height());
+                cols[1].group(|ui| {
+                    ui.heading("Assignments");
+                    for entry in &self.context {
+                        ui.monospace(format!("{} = {}", entry.0, display_expr(entry.1)));
+                    }
+                    ui.set_height(ui.available_height());
+                });
             });
         });
     }
-
-    fn name(&self) -> &str {
-        "GUI Calculator - m_calc"
-    }
 }
-
-fn main() {
+fn main() -> eframe::Result<()> {
     let app = Calculator::new();
     let win_option = NativeOptions::default();
 
-    run_native(Box::new(app), win_option);
+    run_native(
+        "GUI Calculator - m_calc",
+        win_option,
+        Box::new(|_cc| Box::new(app)),
+    )
 }
