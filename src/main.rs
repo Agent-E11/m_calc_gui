4627938880b5561402abede612615fc91523da9b
Eamon <eamon.r.burns@gmail.com>
2023-11-29 23:15:24 -0800

    - Added clear buttons - Added "clear" buttons next to the `Assignments` and `History` headings. They clear all history and assignments Added "clear entry" buttons next to each entry in `Assignments` and `History`. They delete individual entries

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
-// TODO: Add `Clear History` and `Clear Assignments` buttons
 use std::collections::HashMap;
 
 use eframe::{
     run_native,
-    App,
-    Frame,
     NativeOptions,
-    egui::{
-        CentralPanel,
-        Context,
-        Key,
-        Layout,
-        Align,
-        Direction,
-        ScrollArea, TextEdit, vec2,
-    },
+    Frame,
+    App,
+};
+use eframe::egui::{
+    CentralPanel,
+    ScrollArea,
+    TextEdit,
+    Context,
+    Layout,
+    Align,
+    Key,
 };
 
 use m_calc::{
+    display_expr,
     calculate,
     tokenize,
-    display_expr,
-    Token,
+};
+use m_calc::{
     CalcErr,
+    Token,
 };
 
 struct Calculator {
@@ -37,12 +38,12 @@ impl Calculator {
             context: HashMap::new(),
             input_text: String::new(),
             output_text: String::new(),
-            history: vec![vec![]],
+            history: vec![],
         }
     }
 }
 impl App for Calculator {
-    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
         CentralPanel::default().show(ctx, |ui| {
             ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
 
@@ -72,37 +73,58 @@ impl App for Calculator {
                 }
             });
 
+            // Output section
             ui.add_space(5.0);
-            ui.group(|ui| { // Output
+            ui.group(|ui| {
                 ui.label(&self.output_text);
                 ui.set_width(ui.available_width());
             });
 
+            // `History` and `Assignments` columns
             ui.add_space(5.0);
             ui.columns(2, |cols| {
                 cols[0].group(|ui| {
-                    ui.heading("History");
-                    ScrollArea::vertical().id_source("l_scroll").show(ui, |ui| {
-                        for expr in self.history.iter().rev() {
-                            ui.monospace(display_expr(expr));
+                    ui.horizontal_wrapped(|ui| {
+                        ui.heading("History");
+                        if ui.button("Clear").clicked() {
+                            self.history.clear();
                         }
                     });
-                    ui.set_height(ui.available_height());
+                    ScrollArea::vertical().id_source("l_scroll").show(ui, |ui| {
+                        for expr in self.history.clone().iter().enumerate().rev() {
+                            ui.horizontal(|ui| {
+                                if ui.small_button("X").clicked() {
+                                    self.history.remove(expr.0);
+                                }
+                                ui.monospace(display_expr(expr.1));
+                            });
+                        }
+                    });
+                    ui.set_min_size(ui.available_size());
                 });
-                cols[0].set_min_height(cols[0].available_height());
                 cols[1].group(|ui| {
-                    ui.heading("Assignments");
+                    ui.horizontal_wrapped(|ui| {
+                        ui.heading("Assignments");
+                        if ui.button("Clear").clicked() {
+                            self.context.clear();
+                        }
+                    });
                     ScrollArea::vertical().id_source("r_scroll").show(ui, |ui| {
-                        let mut entries: Vec<String> = Vec::new();
+                        let mut entries: Vec<(String, String)> = Vec::new();
                         for entry in &self.context {
-                            entries.push(format!("{} = {}", entry.0, display_expr(entry.1)));
+                            entries.push((entry.0.clone(), format!("{} = {}", entry.0, display_expr(entry.1))));
                         }
                         entries.sort();
                         for entry in entries {
-                            ui.monospace(entry);
+                            ui.horizontal(|ui| {
+                                if ui.button("X").clicked() {
+                                    self.context.remove(&entry.0);
+                                };
+                                ui.monospace(entry.1);
+                            });
                         }
                     });
-                    ui.set_height(ui.available_height());
+                    ui.set_min_size(ui.available_size());
                 });
             });
         });
